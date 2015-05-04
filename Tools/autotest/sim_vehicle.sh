feabda80715173874d4f937b93d1b5cd93c2b53c
Jason Lines <jlines@solidstatenetworks.com>
2015-05-03 17:35:08 -0700

    set local start point

--- Tools/autotest/sim_vehicle.sh
+++ Tools/autotest/sim_vehicle.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # home location lat, lon, alt, heading
-LOCATION="CMAC"
-TRACKER_LOCATION="CMAC_PILOTSBOX"
+LOCATION="jlines"
+TRACKER_LOCATION="jlines"
 VEHICLE=""
 BUILD_TARGET="sitl"
 FRAME=""
